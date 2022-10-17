3edc8be83b6b622244267d2cf848d05bc2735a84
Seann-Moser <seannmoser@gmail.com>
2022-10-17 14:32:26 -0700

    chore: fix unit tests

--- examples/dataset_example.go
+++ examples/dataset_example.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Seann-Moser/QueryHelper/table"
 	"go.uber.org/zap"
 	"log"
+	"time"
 )
 
 type User struct {
@@ -32,7 +33,7 @@ func DatasetExample() {
 	ctx := context.Background()
 
 	// set sqlx db to enable full functionality
-	ds, err := dataset.New(ctx, "default", true, false, logger, nil, User{}, UserSettings{})
+	ds, err := dataset.New(ctx, "default", true, false, logger, nil, dataset.NewGoCache(context.Background(), 5*time.Second, logger), User{}, UserSettings{})
 	if err != nil {
 		logger.Fatal("failed creating dataset")
 	}
